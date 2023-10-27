1890df29198c76e53cc5dffdb412189b543f4a01
arokosandria <kmsa@interia.pl>
2023-10-27 14:34:39 +0200

    solution

---
+++ src/main/java/pl/javastart/task/Point.java
@@ -0,0 +1,17 @@
+package pl.javastart.task;
+
+public class Point {
+    int x;
+    int y;
+
+    @Override
+    public String toString() {
+        return "Punkt(" + x + ", " + y + ")";
+
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
