7e621cc5f3dc1eb4d6dcabcd7fdd72fe3faa9849
arokosandria <kmsa@interia.pl>
2023-10-27 14:36:24 +0200

    solution

--- src/main/java/pl/javastart/task/CoordinateSystem.java
+++ src/main/java/pl/javastart/task/CoordinateSystem.java
@@ -16,15 +16,15 @@ public class CoordinateSystem {
     String viewPoint(Point point) {
         if ((point.x == 0) && (point.y == 0)) {
             return point + " leży na środku układu współrzędnych";
-        } else if (((point.x == 0) && (point.y != 0))) {
+        } else if ((point.x == 0) && (point.y != 0)) {
             return point + " leży na osi Y";
-        } else if (((point.x != 0) && (point.y == 0))) {
+        } else if ((point.x != 0) && (point.y == 0)) {
             return point + " leży na osi X";
-        } else if (((point.x > 0) && (point.y > 0))) {
+        } else if ((point.x > 0) && (point.y > 0)) {
             return point + " leży w I ćwiartce układu współrzędnych";
-        } else if (((point.x > 0) && (point.y < 0))) {
+        } else if ((point.x > 0) && (point.y < 0)) {
             return point + " leży w IV ćwiartce układu współrzędnych";
-        } else if (((point.x < 0) && (point.y < 0))) {
+        } else if ((point.x < 0) && (point.y < 0)) {
             return point + " leży w III ćwiartce układu współrzędnych";
         } else {
             return point + " leży w II ćwiartce układu współrzędnych";
